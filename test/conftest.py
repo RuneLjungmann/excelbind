0ef2d99b61ca27d5e2a79bf1f80bb497d6d50a94
Rune Ljungmann <53918463+RuneLjungmann@users.noreply.github.com>
2019-09-30 15:02:18 +0200

    Autodetect platform in tests

--- test/conftest.py
+++ test/conftest.py
@@ -1,7 +1,12 @@
 import pathlib
 import pytest
+import struct
+
+
+def interpreter_platform():
+    return 'x64' if struct.calcsize('P') == 8 else 'Win32'
 
 
 @pytest.fixture(scope='module')
 def xll_addin_path():
-    return str(pathlib.Path(__file__).parent / '..' / 'Binaries' / 'x64' / 'Release' / 'excelbind.xll')
+    return str(pathlib.Path(__file__).parent / '..' / 'Binaries' / interpreter_platform() / 'Release' / 'excelbind.xll')
